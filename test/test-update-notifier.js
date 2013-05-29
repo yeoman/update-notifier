f2e2f5425a79e3e648a7c71b1f69a613955ed92a
Stephen Sawchuk <sawchuk@gmail.com>
2013-05-29 14:25:29 -0400

    support callbacks.

--- test/test-update-notifier.js
+++ test/test-update-notifier.js
@@ -5,21 +5,31 @@ var fs = require('fs');
 var updateNotifier = require('../lib/update-notifier');
 
 describe('updateNotifier', function() {
-	var settings = {
-		packageName: 'yeoman',
-		packageVersion: '0.9.3'
+	var generateSettings = function (options) {
+		options = options || {};
+		return {
+			packageName: options.packageName || 'generator-backbone',
+			packageVersion: options.packageVersion || '0.1.0',
+			callback: options.callback || null
+		};
 	};
 
-	var configstorePath = updateNotifier(settings).config.path;
+	var configstorePath = updateNotifier(generateSettings()).config.path;
 
 	afterEach(function() {
 		fs.unlinkSync(configstorePath);
 	});
 
 	it('should check for update', function(cb) {
-		updateNotifier(settings).checkNpm(function(error, update) {
-			assert.equal(update.current, '0.9.3');
+		updateNotifier(generateSettings()).checkNpm(function(error, update) {
+			assert.equal(update.current, '0.1.0');
 			cb();
 		});
+	});
+
+	it('should check for update with callback', function(cb) {
+		updateNotifier(generateSettings({
+			callback: cb
+		}));
 	});
 });
