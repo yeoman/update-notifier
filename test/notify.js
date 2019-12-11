cd86e3f7e9eddea9b61b2469d87ce29aaf5fecef
Federico Brigante <opensource@bfred.it>
2019-12-11 22:20:28 +0700

    Add failing test

--- test/notify.js
+++ test/notify.js
@@ -83,3 +83,11 @@ test('should ouput if running as npm script and shouldNotifyInNpmScript option s
 	notifier.notify({defer: false});
 	t.true(stripAnsi(errorLogs).includes('Update available'));
 });
+
+test('should not output if current version is the latest', t => {
+	setupTest(true);
+	const notifier = new Control(true);
+	notifier.packageVersion = '1.0.0';
+	notifier.notify({defer: false});
+	t.false(stripAnsi(errorLogs).includes('Update available'));
+});
