83fd1f2681de64cc9b34144e20535ec256903cba
chris <chris@bumblehead.com>
2022-07-19 08:12:18 -0700

    per review, use undefined rathr than null

--- test/update-notifier.js
+++ test/update-notifier.js
@@ -30,7 +30,7 @@ test.afterEach(() => {
 });
 
 test('fetch info', async t => {
-	const updateNotifier = await esmock('../index.js', null, {'is-ci': false});
+	const updateNotifier = await esmock('../index.js', undefined, {'is-ci': false});
 	configstorePath = updateNotifier(generateSettings()).config.path;
 	const update = await updateNotifier(generateSettings()).fetchInfo();
 	console.log(update);
@@ -38,14 +38,14 @@ test('fetch info', async t => {
 });
 
 test('fetch info with dist-tag', async t => {
-	const updateNotifier = await esmock('../index.js', null, {'is-ci': false});
+	const updateNotifier = await esmock('../index.js', undefined, {'is-ci': false});
 	configstorePath = updateNotifier(generateSettings()).config.path;
 	const update = await updateNotifier(generateSettings({distTag: '0.0.3-rc1'})).fetchInfo();
 	t.is(update.latest, '0.0.3-rc1');
 });
 
 test('don\'t initialize configStore when NO_UPDATE_NOTIFIER is set', async t => {
-	const updateNotifier = await esmock('../index.js', null, {'is-ci': false});
+	const updateNotifier = await esmock('../index.js', undefined, {'is-ci': false});
 	configstorePath = updateNotifier(generateSettings()).config.path;
 	process.env.NO_UPDATE_NOTIFIER = '1';
 	const notifier = updateNotifier(generateSettings());
@@ -53,7 +53,7 @@ test('don\'t initialize configStore when NO_UPDATE_NOTIFIER is set', async t => 
 });
 
 test('don\'t initialize configStore when --no-update-notifier is set', async t => {
-	const updateNotifier = await esmock('../index.js', null, {'is-ci': false});
+	const updateNotifier = await esmock('../index.js', undefined, {'is-ci': false});
 	configstorePath = updateNotifier(generateSettings()).config.path;
 	process.argv.push('--no-update-notifier');
 	const notifier = updateNotifier(generateSettings());
@@ -62,7 +62,7 @@ test('don\'t initialize configStore when --no-update-notifier is set', async t =
 
 test('don\'t initialize configStore when NODE_ENV === "test"', async t => {
 	process.env.NODE_ENV = 'test';
-	const updateNotifier = await esmock('../index.js', null, {'is-ci': false});
+	const updateNotifier = await esmock('../index.js', undefined, {'is-ci': false});
 	const notifier = updateNotifier(generateSettings());
 	t.is(notifier.config, undefined);
 });
